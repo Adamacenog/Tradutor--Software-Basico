ca0b87d2e41b6c4c61dc28c3021adbf3f02c67f7
Jonatas Senna <jonrsp96@gmail.com>
2018-11-19 23:00:28 -0200

    inputs compilando

--- src/input.asm
+++ src/input.asm
@@ -1,79 +1,86 @@
 
 section .text
 
-
-
 leerInteiro:
-  enter 14,0
-  mov dword ebp-5 , 0       ;Inicia o valor da variavel local acumuladora
-  mov db ebp-6 , 0x3        ;inicia o valor da flag
-  mov dword ebp-10, [ebp+8] ;endereço de retorno
-  mov dword ebp-14, 0       ; contador
+  enter 16,0
+  mov dword[ebp-8], 0x0
+  mov  dword[ebp-12], 0x03           ;inicia o valor da flag
+  mov dword[ebp-16], 0       ; contador
 loop1:
+  mov [ebp-4], ebp
+  sub dword[ebp-4], 0x1
   mov ebx, 0                ;Descritor do arquivo (stdin)
-  mov ecx, db ebp-1
+  mov ecx, [ebp-4]
   mov edx, 0x1
   mov eax, 3                ;Número de chamada do sistema (sys_read)
   int 0x80                  ;Chamada do kernel
-  cmp db[ebp-1], 0x2D
+  cmp dword[ebp-4], 0x2D
   jne mark1                 ; Se não começar com - mark1
-  add dword [ebp-14], 0x1     ;contador++
-  mov db ebp-6, 0x1                 ; flag de numero negativo
+  add dword [ebp-16], 0x1     ;contador++
+  mov dword[ebp-12], 0x1                 ; flag de numero negativo
   jmp loop1
 mark1:
-  cmp db[ebp-6],0x3         ; Se a flag não for marcada
+  cmp dword[ebp-12],0x3         ; Se a flag não for marcada
   jne mark2
-  mov db ebp-6, 0x2                 ; flag de numero positivo
+  mov dword[ebp-12], 0x2                 ; flag de numero positivo
 mark2:
-  cmp db[ebp-1], 0xA
+  cmp dword[ebp-4], 0xA
   je  operations                      ;Se for enter
-  add dword [ebp-14], 0x1     ;contador++
-  sub db[ebp-1], 0x30
-  push dword[ebp-5]
-  shl dword[ebp-5], 3 ;aux4 x 10
+  add dword [ebp-16], 0x1     ;contador++
+  sub dword[ebp-4], 0x30
+  push dword[ebp-8]
+  shl dword[ebp-8], 3 ;aux4 x 10
   pop eax
-  add dword[ebp-5], eax
-  add dword[ebp-5], eax
-  add dword[ebp-5], db[ebp-1]
+  add dword[ebp-8], eax
+  add dword[ebp-8], eax
+  mov dword eax, [ebp-4]
+  add dword[ebp-8], eax
   jmp loop1
 operations:
-  cmp db[ebp-6],0x2
+  cmp dword[ebp-12],0x2
   je retorno1 ; é um numero positivo
   ;é negativo, então complemento a 2
-  not dword[ebp-5] ;inverte
-  add dword[ebp-5], 0x1 ; soma 1
+  not dword[ebp-8] ;inverte
+  add dword[ebp-8], 0x1 ; soma 1
 retorno1:
-  mov dword [ebp-10], dword [ebp-5] ;resultado
-  mov eax, dword [ebp-14] ;contador
+  mov dword ebx, ebp
+  add dword [ebx], 0x08
+  mov dword ebx, [ebp-8] ;resultado
+  mov dword ebx, [ebp-16] ;contador
   leave
 
 
 leerChar:
-  enter 5,0
-  mov dword ebp-5, [ebp+8]
+  enter 8,0
+  mov [ebp-4], ebp
+  sub dword [ebp-4], 0x01
   mov ebx, 0                ;Descritor do arquivo (stdin)
-  mov ecx, db ebp-1
+  mov ecx, [ebp-4]
   mov edx, 0x1
   mov eax, 3                ;Número de chamada do sistema (sys_read)
   int 0x80
-  mov dword[ebp-5], db [ebp-1] ;resultado
-  mov eax, 0x1; count
+  mov eax,[ebp+8]
+  mov dword eax, [ebp-1] ;resultado
+  mov dword [eax], 0x1; count
   leave
 
 leerString:
-  enter 9,0
-  mov dword ebp-4, 0
-  mov dword ebp-8, [ebp+8]
+  enter 8,0
+  mov dword [ebp-4], 0
+  push dword [ebp+8]
 loop2:
-  push dword ebp-9 ;endereço de retorno do char
+  mov dword [ebp-8], ebp
+  sub dword [ebp-8], 0x0000000C
+  push dword [ebp-8] ;endereço de retorno do char
   call leerChar
   pop eax ;só pra jogar fora
-  cmp [ebp-9], 0xA ;se for enter pula
+  cmp dword [ebp-8], 0x0A ;se for enter pula
   je retorno2
-  mov db [ebp-8], eax
-  add [ebp-8], 0x1 ;aumenta o endereço em 1B
-  add [ebp-4], 0x1 ; contador ++
-  cmp [ebp-4], 0x64 ;limite máximo de 100 caracteres
+  pop eax
+  mov dword eax, [ebp-8]
+  add dword [eax], 0x1 ;aumenta o endereço em 1B
+  add dword[ebp-4], 0x1 ; contador ++
+  cmp dword [ebp-4], 0x64 ;limite máximo de 100 caracteres
   jbe loop2
 retorno2:
   mov eax, dword[ebp-4]
